dc4caa6695a7f215c9fb47262cd2e4ac32e8ecc7
Elvis <elvisi.muchasaj@gmail.com>
2019-08-08 15:04:59 +0200

    Added some gulp BrowserSync and Css stuff

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp 	= require('gulp'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars 	= require('postcss-simple-vars'),
+	nested 		= require('postcss-nested'),
+	cssImport 	= require('postcss-import');
+
+/* GULP PostCSS Filters */
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer])) //post css filters
+	.on('error', function (errorInfo) {
+		console.log(errorInfo.toString());
+		this.emit('end'); //this task, tell(emit) to gulp that did what it could :/ , and bring things to an end.
+	})
+	.pipe(gulp.dest('./app/temp/styles'));
+});
---
+++ gulp/tasks/watch.js
@@ -0,0 +1,32 @@
+var gulp 	= require('gulp'),
+	watch 	= require ('gulp-watch'),
+	browserSync = require('browser-sync').create();
+
+/* GULP WATCH TASK */
+gulp.task('watch', function() {
+
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app" 
+			/*since gulpfile.js is in our root travel-site, we need to tell browserSync to go to app folder because thats where our  index.html file lives. */
+		}
+	});
+
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('cssInject');
+	})
+
+});
+
+/* GULP CSS Auto Inject  TASK */
+/*Before we run cssInject, we run any dependencies (which is the second argument). styles task is a dependencie of cssInject task  SHORT: Anytime we save a change to any css file, we are trigering the cssInject task, and we've build the cssInject task in such a way that, it wont even begin, untill the styles task has a chance to run and complete, so the compiled css file would have a chanece to be generated. AND only at that point we will pipe it in to browserSync. */
+
+gulp.task('cssInject',['styles'] ,function(){
+	return gulp.src('./app/temp/styles/styles.css')
+	.pipe(browserSync.stream());
+});

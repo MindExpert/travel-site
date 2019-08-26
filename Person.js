34d8adb70091e85a049aa0116bd6fa8244837175
Elvis <elvisi.muchasaj@gmail.com>
2019-08-26 16:13:59 +0200

    Added class functionalities

---
+++ Person.js
@@ -0,0 +1,36 @@
+// class Person {
+// 	constructor(fullName, favColor){
+// 		this.name = fullName;
+// 		this.color = favColor;
+// 	}
+// 	greet() {
+// 		console.log("Hi There, My name is " + this.name + " and my favorite color i " + this.color + ".");
+// 	}
+// }
+// export default Person;
+
+
+
+// function Person(fullName, favColor) {
+// 	this.name = fullName;
+// 	this.color = favColor;
+// 	this.greet = function() { //method
+// 		console.log("Hello There, My name is " + this.name + " and my favorite color i " + this.color + ".");
+// 	}
+// }
+// module.exports = Person;
+
+/* WebPack Testing*/
+// console.log("Baby Boy");
+// exports.exampleProperty = "Super Magical time ladies...";
+// exports.exampleFunction = function () {
+// 	alert("This is an Example.");
+// }
+// 
+// 
+/* WebPack APP.js Testing */ 
+//var Person = require('./modules/Person'); // When use require (part of NODE.js), you dont need to use .js
+//var $ = require('jquery');
+//$("h1").remove();
+// console.log(Person.exampleProperty);
+// Person.exampleFunction();
--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,25 +1,3 @@
-//var Person = require('./modules/Person'); // When use require (part of NODE.js), you dont need to use .js
-//var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in texas.")
-	}
-}
-
-
-var john = new Person("John Doe", "Black");
-	john.greet();
-
-var jane = new Adult("Jane Smith", "WHITE");
-	jane.greet();
-	jane.payTaxes();
-
-
-
-
-/* WebPack Testing */ 
-//$("h1").remove();
-// console.log(Person.exampleProperty);
-// Person.exampleFunction();
+var mobileMenu = new MobileMenu();

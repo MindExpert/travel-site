d61e412c3a5a30495ff4644621ca7b5a25bce9e9
Elvis <elvisi.muchasaj@gmail.com>
2019-08-30 12:37:49 +0200

    Fixing bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@ var path = require('path');
 
 module.exports = { //tell webpack at which file it should look to make our bundle
 	// mode: 'none', or 'development' or 'production'
-	entry: "./app/assets/scripts/App.js",
+	entry: {
+		App: "./app/assets/scripts/App.js",
+		Vendor: "./app/assets/scripts/Vendor.js"
+	},
 	output: { //where we want the bundled fille to be created (Needs an absolute path)
 		path: path.resolve(__dirname, "./app/temp/scripts"),
-		filename: "App.js"
+		filename: "[name].js"
 	},
 	module: {
 		rules: [

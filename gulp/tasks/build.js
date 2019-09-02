8ca4d1e815784c3a0539aefda4b056986d925f92
Elvis <elvisi.muchasaj@gmail.com>
2019-09-02 12:07:30 +0200

    Completed gulp Build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	del = require('del'),
+	usemin = require('gulp-usemin'),
+	rev = require('gulp-rev'),
+	cssnano = require('gulp-cssnano'),
+	uglify = require('gulp-uglify'),
+	browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function() {
+	browserSync.init({
+		notify: true,
+		server: {
+			baseDir: "docs" 
+			/* since gulpfile.js is in our root travel-site, we need to tell browserSync to go to app folder because thats where our  index.html file lives. */
+		}
+	});
+});
+
+gulp.task('deleteDistFolder', function(){
+	return del("./docs");
+});
+
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
+	var pathsToCopy = [
+		'./app/**/*',
+		'!./app/index.html',
+		'!./app/assets/images/**',
+		'!./app/assets/styles/**',
+		'!./app/assets/scripts/**',
+		'!./app/temp',
+		'!./app/temp/**'
+	];
+	return gulp.src(pathsToCopy)
+		.pipe(gulp.dest("./docs"));
+});
+
+gulp.task('optimizeImages', ['deleteDistFolder'], function(){
+ // return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/i			progressive: tcons./**/*']) !-Excludes a Folder/File
+	return gulp.src('./app/assets/images/**/*')
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true,
+			multipass: true
+		}))
+		.pipe(gulp.dest("./docs/assets/images"));
+});
+
+gulp.task('useminTrigger', ['deleteDistFolder'], function() {
+	gulp.start("usemin");
+})
+
+gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function(){
+	return gulp.src('./app/index.html')
+		.pipe(usemin({
+			css: [function() {return rev()}, function() {return cssnano()}],
+			js: [function() {return rev()}, function() {return uglify()}]
+		}))
+		.pipe(gulp.dest("./docs"));
+})
+
+
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
